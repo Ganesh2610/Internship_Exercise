82189ed4034fb2ac23d65f6f05a7395db69ec9a6
Ganesh2610 <ganeshbs9529@gmail.com>
2024-01-17 12:17:30 +0530

    Encapsulation demo

---
+++ dart_demo/lib/oops_in_dart/encapsulation.dart
@@ -0,0 +1,30 @@
+class Employee {
+  int? _id;
+  String? _name;
+
+  int getId() {
+    return _id!;
+  }
+
+  String getName() {
+    return _name!;
+  }
+
+  void setId(int id) {
+    _id = id;
+  }
+
+  void setName(String name) {
+    _name = name;
+  }
+}
+
+void main() {
+  Employee emp = Employee();
+
+  emp.setId(1);
+  emp.setName("John");
+
+  print("Id: ${emp.getId()}");
+  print("Name: ${emp.getName()}");
+}
