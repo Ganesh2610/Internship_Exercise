227c54f6aeb88806baca559b73d9916274468f56
Ganesh2610 <ganeshbs9529@gmail.com>
2024-01-17 16:15:31 +0530

    Mixins in dart

---
+++ dart_demo/lib/oops_in_dart/mixins_in_dart.dart
@@ -0,0 +1,29 @@
+//we can implement multiple inheritance using mixins.
+mixin CanFly {
+  void fly() {
+    print('I can fly');
+  }
+}
+
+mixin CanWalk {
+  void walk() {
+    print('I can walk');
+  }
+}
+
+class Bird with CanFly, CanWalk {
+ 
+}
+
+class Human with CanWalk {
+ 
+}
+
+void main() {
+  var bird = Bird();
+  bird.fly();
+  bird.walk();
+
+  var human = Human();
+  human.walk();
+}
